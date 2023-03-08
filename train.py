d99a25225e6ceb84677a5de4bdda7ad4def3bdff
Vsevolod Doronin <vsevolod.doronin@yandex.ru>
2023-03-09 00:23:14 +0500

    add 2

--- train.py
+++ train.py
@@ -1,4 +1,13 @@
 from ultralytics import YOLO
 
-model = YOLO("yolov8m.pt")  # load a pretrained model (recommended for training)
-model.train(data='data.yaml',epochs=20, name='yolov8m')
+model = YOLO("yolov8n.pt")  # load a pretrained model (recommended for training)
+model.train(data='data.yaml',epochs=30, name='yolov8n', batch=16, save_period=5)
+
+model = YOLO("yolov8s.pt")  # load a pretrained model (recommended for training)
+model.train(data='data.yaml',epochs=30, name='yolov8s', batch=16, save_period=5)
+
+model = YOLO("yolov8n.pt")  # load a pretrained model (recommended for training)
+model.train(data='data.yaml',epochs=50, name='yolov8n', batch=8, save_period=5)
+
+model = YOLO("yolov8s.pt")  # load a pretrained model (recommended for training)
+model.train(data='data.yaml',epochs=50, name='yolov8s', batch=8, save_period=5)
